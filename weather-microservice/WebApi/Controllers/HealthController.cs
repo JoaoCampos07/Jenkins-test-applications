9eedcc5f2384838a8351821a9ecf58cd4a59de3b
Joao Campos <49458268+JoaoCampos07@users.noreply.github.com>
2022-12-04 15:47:51 +0100

    Add Liveliness and Readiness endpoints

---
+++ weather-microservice/WebApi/Controllers/HealthController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    public class HealthController : Controller
+    {
+        [HttpGet]
+        [Route("readiness")]
+        public ActionResult Readiness()
+        {
+            return Ok("Ready");
+        }
+
+        [HttpGet]
+        [Route("liveliness")]
+        public ActionResult Liveliness(int id)
+        {
+            return Ok("ALive");
+        }
+    }
+}
